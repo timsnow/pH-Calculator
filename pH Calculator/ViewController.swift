6c8e9d551dffe302f38b4bd603fc2ff7edfbb08c
Tim Snow <tim.snow@cunninglemon.com>
2015-05-22 22:48:52 +0100

    Initial Commit

---
+++ pH Calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  pH Calculator
+//
+//  Created by Tim Snow on 22/05/2015.
+//  Copyright (c) 2015 Tim Snow. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
